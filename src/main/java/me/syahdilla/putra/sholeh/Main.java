340de6710b467f8db1eae481caf0b44cf7743db6
Putra <syahdilla.ps@gmail.com>
2023-03-18 08:33:11 +0700

    call p3 classes

--- src/main/java/me/syahdilla/putra/sholeh/Main.java
+++ src/main/java/me/syahdilla/putra/sholeh/Main.java
@@ -1,11 +1,17 @@
 package me.syahdilla.putra.sholeh;
 
 import me.syahdilla.putra.sholeh.semester2.p2.DataDiri;
+import me.syahdilla.putra.sholeh.semester2.p3.KelilingPersegiPanjang;
+import me.syahdilla.putra.sholeh.semester2.p3.OperatorAritmatika;
+import me.syahdilla.putra.sholeh.semester2.p3.VariabelOperator;
 
 public class Main {
 
     public static void main(String[] args) {
         DataDiri.main(args);
+        KelilingPersegiPanjang.main(args);
+        OperatorAritmatika.main(args);
+        VariabelOperator.main(args);
     }
 
 }
