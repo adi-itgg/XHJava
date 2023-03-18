87e8143102d7118acf18705b1edb536649c81f69
Putra <syahdilla.ps@gmail.com>
2023-03-18 09:01:21 +0700

    Add latihan convert thermal

---
+++ src/main/java/me/syahdilla/putra/sholeh/semester2/p3/LatihanConvertThermal.java
@@ -0,0 +1,21 @@
+package me.syahdilla.putra.sholeh.semester2.p3;
+
+/**
+ * Convert celcius to farenheit, reamur and kelvin
+ */
+public class LatihanConvertThermal {
+
+    public static void main(String[] args) {
+        int celcius = 40;
+
+        double kelvin = celcius + 273.15;
+        double farenheit = (9.0/5.0) * celcius;
+        double reamur  = (4.0/5.0) * celcius;
+
+        System.out.println("Celcius: " + celcius);
+        System.out.println("Celcius ke kelvin: " + kelvin);
+        System.out.println("Celcius ke farenheit: " + farenheit);
+        System.out.println("Celcius ke reamur: " + reamur);
+    }
+
+}
