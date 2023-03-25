80ac0f25dd8cc417e93b0a8e9cc2720da648b2b0
Putra <syahdilla.ps@gmail.com>
2023-03-25 10:54:48 +0700

    add p4 - ConvertThermalInp with Scanner

--- src/main/java/me/syahdilla/putra/sholeh/Main.java
+++ src/main/java/me/syahdilla/putra/sholeh/Main.java
@@ -4,6 +4,7 @@ import me.syahdilla.putra.sholeh.semester2.p2.DataDiri;
 import me.syahdilla.putra.sholeh.semester2.p3.KelilingPersegiPanjang;
 import me.syahdilla.putra.sholeh.semester2.p3.OperatorAritmatika;
 import me.syahdilla.putra.sholeh.semester2.p3.VariabelOperator;
+import me.syahdilla.putra.sholeh.semester2.p4.ConvertThermalInp;
 import me.syahdilla.putra.sholeh.semester2.p4.OperatorTunggal;
 
 public class Main {
@@ -14,6 +15,7 @@ public class Main {
         OperatorAritmatika.main(args);
         VariabelOperator.main(args);
         OperatorTunggal.main(args);
+        ConvertThermalInp.main(args);
     }
 
 }
