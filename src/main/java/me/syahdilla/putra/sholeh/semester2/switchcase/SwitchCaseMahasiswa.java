ded66ff6735d5584a97c3eb0684dbc1c78df3183
Putra <syahdilla.ps@gmail.com>
2023-05-13 09:01:15 +0700

    Add switch case

---
+++ src/main/java/me/syahdilla/putra/sholeh/semester2/switchcase/SwitchCaseMahasiswa.java
@@ -0,0 +1,25 @@
+package me.syahdilla.putra.sholeh.semester2.switchcase;
+
+import java.util.Scanner;
+
+public class SwitchCaseMahasiswa {
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.print("Input nilai: ");
+        var nilai = scanner.nextInt();
+        var grade = "E";
+        scanner.close();
+        switch (nilai) {
+            case 91, 92, 94, 93, 95, 96, 97, 98, 99, 100 -> grade = "A+";
+            case 81, 82, 83, 84, 85, 86, 87, 88, 89, 90 -> grade = "A";
+            case 71, 72, 73, 74, 75, 76, 77, 78, 79, 80 -> grade = "B";
+            case 61, 62, 63, 64, 65, 66, 67, 68, 69, 70 -> grade = "C";
+            case 51, 52, 53, 54, 55, 56, 57, 58, 59, 60 -> grade = "D";
+            default -> grade = "?";
+        }
+
+        System.out.println("Nilai: " + nilai);
+        System.out.println("Grade: " + grade);
+    }
+}
